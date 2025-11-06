28024b5a174580de4216c7b2e31d0eb1c1dd758a
Raki <57454088+ika-musume@users.noreply.github.com>
2025-11-06 22:11:26 +0900

    update 20251106

---
+++ resources/src/IKA9958_rcc.sv
@@ -0,0 +1,10 @@
+module IKA9958_rcc #(parameter CM = 1) (
+
+);
+
+/*
+    IKA9958 Reset and Clock Control
+*/
+
+
+endmodule
---
+++ resources/src/IKA9958_st.sv
@@ -0,0 +1,17 @@
+module IKA9958_st (
+
+);
+
+/*
+    IKA9958 Screen Timing
+    This module generates all video timing signals
+*/
+
+
+
+
+
+
+
+
+endmodule
